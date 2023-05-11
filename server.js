36295aef9b99fd20ed16399dbe26e3348b673922
dnh <rootdnh@gmail.com>
2023-05-11 16:54:53 -0300

    create password validator

--- server.js
+++ server.js
@@ -18,28 +18,68 @@ server.get("/", (req, res) => {
  res.status(200).json({ msg: "Welcome to auth api" });
 });
 
-server.get("/auth/register", async (req, res) => {
- const { name, mail, pass, confirm_pass } = req.body;
+server.post("/auth/register", async (req, res) => {
+ const { name, email, password, confirm_pass } = req.body;
 
  if (!name) {
   res.status(422).json({ error: true, msg: "Name is a required field" });
  }
- if (!mail) {
+ if (!email) {
   res.status(422).json({ error: true, msg: "Mail is a required field" });
  }
- if (!pass) {
+ if (!password) {
   res.status(422).json({ error: true, msg: "Password is a required field" });
  }
- if (pass !== confirm_pass) {
+ if (password !== confirm_pass) {
   res.status(422).json({ error: true, msg: "Passwords is not match" });
  }
  //Check if user exists
- const existsUser = User.findOne({ email: email });
-
+ const existsUser = await User.findOne({ email: email });
  if (existsUser) {
+  console.log(existsUser);
+  res.status(422).json({ error: true, msg: "This email is already in use" });
+ } else {
+  //Create a password cryptography
+  const salt = await bcrypt.genSalt(11);
+  const passCrypt = await bcrypt.hash(password, salt);
+
+  //Create a new User
+  const newUser = new User({
+   name,
+   email,
+   password: passCrypt,
+  });
+
+  try {
+   await newUser.save();
+   res.status(201).json({ error: false, msg: "User is successfully created" });
+  } catch (error) {
+   res.status(500).json({ error: true, msg: "Internal server error" });
+  }
+ }
+});
+
+server.post("/auth/login", async (req, res) => {
+ const { email, password } = req.body;
+
+ if (!email) {
+  res.status(422).json({ error: true, msg: "Mail is a required field" });
+ }
+ if (!password) {
+  res.status(422).json({ error: true, msg: "Password is a required field" });
  }
 
- res.status(200).json({ name, mail, pass });
+ //Check if user exists
+ const userLogin = await User.findOne({ email: email });
+
+ if (!userLogin) {
+  res.status(404).json({ error: true, msg: "User not found" });
+ } else {
+  const checkPassword = await bcrypt.compare(password, userLogin.password);
+  checkPassword
+   ? res.status(200).json({ error: false, msg: "The user has been successfully logged" })
+   : res.status(422).json({ error: true, msg: "The password is not match" });
+ }
 });
 
 //Connection with database MONGODB ATLAS
